5d885d719233332f090cf0a2ac064835a1ec927d
lyfsn <dev.wangyu@proton.me>
2023-12-04 15:18:16 +0800

    up

---
+++ clear.sh
@@ -0,0 +1,6 @@
+sudo rm -rvf execution-data
+sudo rm -rvf consensus-data
+
+sudo rm -rvf validator-keys/logs
+sudo rm -rvf validator-keys/slashing_protection.sqlite
+sudo rm -rvf validator-keys/slashing_protection.sqlite-journal
