e071a1424ae7f1c48e1ae3523810b948d29e2f7c
lyfsn <dev.wangyu@proton.me>
2023-12-14 18:00:49 +0800

    up

---
+++ exit.sh
@@ -0,0 +1,8 @@
+
+
+
+lighthouse \
+    --testnet-dir=./el-cl-genesis-data/custom_config_data \
+    account validator exit \
+    --keystore ./validator_keys/keystore-m_12381_3600_0_0_0-1701675933.json\
+    --beacon-node http://88.99.94.109:4900
